9f32697204f0a8463043bdf91e1486ed289629b7
kent12t <34294530+kent12t@users.noreply.github.com>
2024-03-20 06:23:13 +0800

    color indicator

--- sketch.js
+++ sketch.js
@@ -1,18 +1,25 @@
 // Variables to store device motion data
-let x, y, z;
+let x = null;
+let y = null;
+let z = null;
 let alpha, beta, gamma;
+let bg;
+
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
+
 }
 
 function draw() {
-    background(220);
     // Map device acceleration to canvas coordinates
     // This is a basic mapping; you might need to adjust the scaling
+    bg = color(255, 0, 0);
 
-    if (x && y) {
-        ellipse(map(x, -10, 10, 0, width), map(y, -10, 10, 0, height), 50, 50);
+    background(bg);
+    if (x != 0 && y != 0) {
+        bg = color(0, 255, 0);
+        ellipse(map(x, -10, 10, 0, windowWidth), map(y, -10, 10, 0, windowHeight), 50, 50);
     }
 }
 
