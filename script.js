fe4977cd204a6f3a3a9f0d1ae2070564bc3e0b3b
kent12t <34294530+kent12t@users.noreply.github.com>
2024-05-08 01:05:47 +0800

    circle changes shape every cycle

--- script.js
+++ script.js
@@ -7,43 +7,82 @@ const lighter = "#f3f0eb";
 const newGreen = "#c2c070";
 const orange = "#de8f54";
 
-window.onload = function () {
-    const container = document.getElementById('svgContainer');
-    fetch('assets/jaggedCircles/0c0u/Vector.svg')
-        .then(response => response.text())
-        .then(data => {
-            container.innerHTML = data;
-            initAnimation();
-        })
-        .catch(error => console.error('Error loading the SVG:', error));
+let currentSVGIndex = 0;
 
-    function initAnimation() {
-        const svgElement = document.querySelector('#svgContainer svg');
+const svgFolders = ['assets/jaggedCircles/0c0u', 'assets/jaggedCircles/0c1u', 'assets/jaggedCircles/1c0u', 'assets/jaggedCircles/1c1u', 'assets/jaggedCircles/1c2u'];
+
+const container = document.getElementById('svgContainer');
+let svgArray = [];
+
+
+
+window.onload = () => {
+
+    for (let i = 0; i < svgFolders.length; i++) {
+        // dynamically fetch folders into the svg arrays
+        for (let j = 0; j < 10; j++) {
+            fetch(svgFolders[i] + `/Vector-${j}.svg`)
+                .then(response => response.text())
+                .then(data => {
+                    svgArray.push(data);
+
+                    if (i == svgFolders.length - 1 && j == 9) {
+                        // console.log('All SVGs loaded', svgArray.length);
+                        // console.log(container.innerHTML);
+                        loadNextSVG();
+                    }
+                })
+                .catch(error => console.error('Error loading the SVG:', error));
+
+        }
+    }
+
+    let currentSVGIndex = 0;
+
+    function loadNextSVG() {
+        currentSVGIndex = returnRandInt(0, svgArray.length - 1);
+        initAnimation(currentSVGIndex);
+    }
+
+    function initAnimation(num) {
+        // const container = document.getElementById('svgContainer');
+        container.innerHTML = svgArray[num];
+
+        const svgElement = container.querySelector('svg');
         const paths = svgElement.getElementsByTagName('path');
-        let step;
+
         Array.from(paths).forEach(path => {
             const dot = document.createElementNS("http://www.w3.org/2000/svg", "circle");
             dot.setAttribute("r", "30");
-            dot.setAttribute("fill", orange);
+            dot.setAttribute("fill", "orange");
             svgElement.appendChild(dot);
 
-            const pathLength = path.getTotalLength();
-            let progress = 0;
-            step = randomSpeed(); // Adjust step size for speed control
-
-            function animateDot() {
-                requestAnimationFrame(animateDot);
-                const point = path.getPointAtLength(progress % pathLength);
-                dot.setAttribute('cx', point.x);
-                dot.setAttribute('cy', point.y);
-                progress += step;
-            }
-
-            animateDot();
+            animatePath(dot, path);
         });
     }
 
-    function randomSpeed() {
-        return Math.random() * 2 + 2;
+    function animatePath(dot, path) {
+        const pathLength = path.getTotalLength();
+        let progress = 0;
+
+        function animateDot() {
+            if (progress < pathLength) {
+                requestAnimationFrame(animateDot);
+                const point = path.getPointAtLength(progress);
+                dot.setAttribute('cx', point.x);
+                dot.setAttribute('cy', point.y);
+                progress += 1;
+            } else {
+                loadNextSVG();
+            }
+        }
+
+        animateDot();
     }
-};
+
+}
+
+function returnRandInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
