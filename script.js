4b5639e98b42166211d279d3d503fc352c72026d
kent12t <34294530+kent12t@users.noreply.github.com>
2024-05-07 20:10:25 +0800

    minimal change, bug: truncated data still sometimes cut off so json parse breaks

--- script.js
+++ script.js
@@ -13,16 +13,40 @@ window.onload = function () {
         }, false);
 
         source.addEventListener('message', function (e) {
-            console.warn("========= message", e.data, "=========");
+            console.info("========= message", e.data, "=========");
         }, false);
 
         source.addEventListener('imu_data', function (e) {  // Listening for 'imu_data'
+            // Parse the JSON data received from the server
             var data = e.data;
-            var arr = data.split(',');
+            // var data = JSON.parse(e.data);
 
             // imuData[16] = { ax, ay, az, gx, gy, gz, mx, my, mz, q0, q1, q2, q3, roll, pitch, heading }
 
-            console.log(arr);
+
+            // data is expected to be an array of objects
+            // data.forEach(function (entry) {
+            //     console.log("Timestamp: " + entry.timestamp);
+            //     console.log("Accelerometer X: " + entry.ax);
+            //     console.log("Accelerometer Y: " + entry.ay);
+            //     console.log("Accelerometer Z: " + entry.az);
+            //     console.log("Gyroscope X: " + entry.gx);
+            //     console.log("Gyroscope Y: " + entry.gy);
+            //     console.log("Gyroscope Z: " + entry.gz);
+            //     console.log("Magnetometer X: " + entry.mx);
+            //     console.log("Magnetometer Y: " + entry.my);
+            //     console.log("Magnetometer Z: " + entry.mz);
+            //     console.log("Quaternion q0: " + entry.q0);
+            //     console.log("Quaternion q1: " + entry.q1);
+            //     console.log("Quaternion q2: " + entry.q2);
+            //     console.log("Quaternion q3: " + entry.q3);
+            //     console.log("Roll: " + entry.roll);
+            //     console.log("Pitch: " + entry.pitch);
+            //     console.log("Heading: " + entry.heading);
+            // });
+
+
+            console.log(data);
         }, false);
     }
 };
